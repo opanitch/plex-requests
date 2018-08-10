aa2e38704be00baaefcfdfec498bc9620cbca322
Oren Panitch <Oreokid200@gmail.com>
2018-08-09 22:32:51 -0400

    one last structure change before dev starts

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 // import PropTypes from 'prop-types';
 
 import Header from '../containers/Header/Header.jsx';
-import RequestList from '../containers/RequestList/RequestList.jsx';
-import RequestForm from '../containers/RequestForm/RequestForm.jsx';
+import RequestList from '../containers/Request/RequestList.jsx';
+import RequestForm from '../containers/Request/RequestForm.jsx';
 import LibraryList from '../containers/Library/LibraryList.jsx';
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="plex-request">
         <Header />
         <RequestList />
         <RequestForm />
