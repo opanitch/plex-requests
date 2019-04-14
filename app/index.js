fde5d4a8d9620f64bfd91c23959f017b43dcf981
Oren Panitch <Oreokid200@gmail.com>
2019-04-14 11:05:18 -0400

    adding changes to index that I missed

--- app/index.js
+++ app/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // main app
-import App from './containers/AppContainer.jsx';
+import AppContainer from './containers/AppContainer.jsx';
 // import configureStore, { history } from 'utils/store/configureStore';
 
 // styles
 import './index.scss';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<AppContainer />, document.getElementById('app'));
