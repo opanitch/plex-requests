f6bbde119694fa41ec55c64f5788fcad414c7f6d
Oren Panitch <Oreokid200@gmail.com>
2019-06-23 19:42:41 -0400

    removing out of scope actions

--- app/api/actions.js
+++
@@ -1,5 +0,0 @@
-import { default as LIBRARY_ACTIONS } from './actions/library';
-import { default as REQUEST_ACTIONS } from './actions/requests';
-import { default as TEST_ACTIONS } from './actions/test-actions';
-
-export default { LIBRARY_ACTIONS, REQUEST_ACTIONS, TEST_ACTIONS };
--- app/api/actions/test-actions.js
+++
@@ -1,9 +0,0 @@
-import { ACTION_TYPES } from '../constants';
-
-export function addArticle(payload) {
-  return { type: ACTION_TYPES.ADD_ARTICLE, payload };
-}
-
-export default {
-  addArticle
-};
