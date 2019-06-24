7b9951645b2f1e3d9ea10ba9a50b296b756a5af6
Oren Panitch <Oreokid200@gmail.com>
2019-06-23 20:25:21 -0400

    fixes to the store setup, and added table work

--- app/api/reducers.js
+++ app/api/reducers.js
@@ -1,5 +1,3 @@
-import { default as LIBRARY_REDUCER } from './reducers/library-list';
-import { default as REQUEST_REDUCER } from './reducers/request-list';
-import { default as TEST_REDUCER } from './reducers/test-reducer';
-
-export default { LIBRARY_REDUCER, REQUEST_REDUCER, TEST_REDUCER };
+export { default as LIBRARY_REDUCER } from './reducers/library-list';
+export { default as REQUEST_REDUCER } from './reducers/request-list';
+export { default as TEST_REDUCER } from './reducers/test-reducer';
--- app/containers/AppContainer.jsx
+++ app/containers/AppContainer.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { Provider } from 'react-redux';
 
 // Store
-import FACTORY from '../api/factory';
+import { STORE } from '../api/factory.js';
 
 // React Components/Container
 import App from '../components/App.jsx';
 
 export const AppContainer = () => {
   return (
-    <Provider store={FACTORY.STORE}>
+    <Provider store={STORE}>
       <App />
     </Provider>
   );
