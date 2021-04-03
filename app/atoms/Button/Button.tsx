3d5d70c64e3ee5e29a5cd1e7ad83365126f427f1
Oren Panitch <oreokid200@gmail.com>
2021-04-03 13:51:50 -0400

    updating Button and Card Atoms, and rethinking arch for Request components

--- app/atoms/Button/Button.tsx
+++ app/atoms/Button/Button.tsx
@@ -16,7 +16,41 @@ const Button: FunctionComponent<ButtonProps> = ({
   disabled = false,
   ...props
 }) => {
-  const buttonClasses = classNames('pr-button', parentClasses);
+  const buttonStates = {
+    base: !disabled,
+    hover: !disabled,
+    disabled: disabled,
+  };
+
+  const buttonClasses = classNames(
+    'pr-button',
+    {
+      // Base State
+      ['']: buttonTheme === ButtonTheme.BASE,
+      ['px-3 py-1 border-2 border-transparent rounded-md']:
+        buttonTheme === ButtonTheme.PRIMARY,
+    },
+    {
+      // Base Theming
+      ['']: buttonStates.base && buttonTheme === ButtonTheme.BASE,
+      ['bg-grey-12 border-white text-white']:
+        buttonStates.base && buttonTheme === ButtonTheme.PRIMARY,
+    },
+    {
+      // Hover State
+      ['']: buttonStates.hover && buttonTheme === ButtonTheme.BASE,
+      ['hover:bg-white hover:border-grey-12 hover:text-grey-12']:
+        buttonStates.hover && buttonTheme === ButtonTheme.PRIMARY,
+    },
+    {
+      // Disabled State
+      ['pointer-events-none cursor-pointer']: buttonStates.disabled,
+      ['']: buttonStates.disabled && buttonTheme === ButtonTheme.BASE,
+      ['bg-grey-10 border-grey-10 text-grey-11']:
+        buttonStates.disabled && buttonTheme === ButtonTheme.PRIMARY,
+    },
+    parentClasses
+  );
 
   return <button className={buttonClasses} disabled={disabled} {...props} />;
 };
